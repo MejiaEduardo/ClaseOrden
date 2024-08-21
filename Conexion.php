bd2033c9733e6558ab922f6c6edaf04009b4fd0c
angeel22m <mmoises.martinez0139@gmail.com>
2024-08-21 10:23:48 -0600

    modified:   Conexion.php 	modified:   Index.html

--- Conexion.php
+++ Conexion.php
@@ -6,14 +6,14 @@ class Connection {
         $dbname = "Prueba";  // Cambia esto por el nombre de tu base de datos
         $host = "localhost";  // Cambia esto si tu base de datos no está en el mismo servidor
         $username = "postgres";  // Cambia esto por tu nombre de usuario de PostgreSQL
-        $password = "12345678";  // Cambia esto por tu contraseña de PostgreSQL
+        $password = "1234";  // Cambia esto por tu contraseña de PostgreSQL
 
         $dsn = "pgsql:host=$host;dbname=$dbname";
 
         try {
             $this->pdo = new PDO($dsn, $username, $password);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Conexión exitosa a la base de datos.";
+           // echo "Conexión exitosa a la base de datos.";
         } catch (PDOException $e) {
             echo "Error de conexión: " . $e->getMessage();
         }
